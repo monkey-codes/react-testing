2ffffc7c29225472f1408af6099fbb763ee520f5
monkey codes <jz@monkey.codes>
2016-11-02 09:19:10 +1100

    Working test setup with sample tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,54 @@
+import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+
+//Purpose:
+// Set up testing environment to run like a browser in the command line
+
+global.document = jsdom.jsdom('<!docktype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = _$(global.window);
 
-chaiJquery(chai, chai.util, $);
-
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+// build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass, props, state) {
+  //this is not the DOM Node!
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
-  );
+
+  )
 
   return $(ReactDOM.findDOMNode(componentInstance));
+
 }
 
+
+// Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
-  if (value) {
+  //inside the function 'this' is an array of selected DOM nodes from the preceding jquery selector.
+  // eg $('div').simulate(...)
+
+  if(value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+  //https://facebook.github.io/react/docs/test-utils.html#simulate
+  TestUtils.Simulate[eventName](this[0]);
+
+}
+
+// Set up chai-jquery
+chaiJquery(chai, chai.util, $);
+
+export {renderComponent, expect}
+
+
